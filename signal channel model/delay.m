0307644020c992450259acd98d3cca483042e007
shinanligao <39136233+shinanligao@users.noreply.github.com>
2018-05-12 19:10:22 +0200

    Update delay.m

--- signal channel model/delay.m
+++ signal channel model/delay.m
@@ -3,25 +3,25 @@ function [ delayed ] = delay( input, A)
 % remaining elements with zeros. The vector grows correspondingly.
 % IMPORTANT: A cannot be larger than the size of input.
 
-    l = length(input);
+l = length(input);
 
-    tail = input(end-A+1:end);
+tail = input(end-A+1:end);
 
-    if (A==1)
-        tail = input(end);
-    end
+if (A==1)
+    tail = input(end);
+end
+
+
+for k = 0:(l-1-A)
+    input(l - k) = input(l - k - A);
+end
+
+for j = 1:A
+    input(j) = 0;
+end
 
     
-    for k = 0:(l-1-A)
-        input(l - k) = input(l - k - A);
-    end
-    
-    for j = 1:A
-        input(j) = 0;
-    end
-    
-    
-    delayed = cat(2, input, tail);
+delayed = cat(2, input, tail);
 
 end
 
