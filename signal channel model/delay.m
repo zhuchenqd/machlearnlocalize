908208fc0d63ca8edd14cd75874a5949167cac9e
shinanligao <39136233+shinanligao@users.noreply.github.com>
2018-05-12 20:34:20 +0200

    Update delay.m
    Input has to be rounded as only shifts by whole nanoseconds are allowed

--- signal channel model/delay.m
+++ signal channel model/delay.m
@@ -1,22 +1,25 @@
 function [ delayed ] = delay( input, A)
-% Summary: shifts an array with values by A to the right and fills the
+% Summary:  Shifts an array with values by A to the right and fills the
 % remaining elements with zeros. The vector grows correspondingly.
 % IMPORTANT: A cannot be larger than the size of input.
 
 l = length(input);
 
-tail = input(end-A+1:end);
+% Round input to nearest integer: Only shifts of whole nanoseconds allowed
+delta = round(A);
 
-if (A==1)
+tail = input(end-delta+1:end);
+
+if (delta==1)
     tail = input(end);
 end
 
 
-for k = 0:(l-1-A)
-    input(l - k) = input(l - k - A);
+for k = 0:(l-1-delta)
+    input(l - k) = input(l - k - delta);
 end
 
-for j = 1:A
+for j = 1:delta
     input(j) = 0;
 end
 
