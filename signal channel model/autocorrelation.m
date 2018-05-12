adae3503cf866a5be9bbe0ff6b0c424dd20acd7b
shinanligao <39136233+shinanligao@users.noreply.github.com>
2018-05-12 18:05:27 +0200

    Update autocorrelation.m

--- signal channel model/autocorrelation.m
+++ signal channel model/autocorrelation.m
@@ -1,14 +1,14 @@
 function [rPhi] = autocorrelation()
-%Summary: Generates the autocorrelation function of r(t)
+% Summary: Generates the autocorrelation function of r(t)
 
 W = 2*499.2 * 10^6; % Bandwidth (Hz)
+global datalength;
 
-%Sampling
-Fs = 2 * W;                                     % Sampling frequency
-t = (-200000):((1/Fs)*10^9):(200000);           % Time vector 
-L = length(t);                                  % Signal length
+% Sampling
+Fs = 2 * W;                                          % Sampling frequency
+t = (-datalength/2):((1/Fs)*10^9):(datalength/2);    % Time vector 
 
-%Autocorrelation function
+% Autocorrelation function
 y = sinc(t/2)*((cos(pi*0.6*t/2))/((1-(0.6*t)).^2))*cos(2*pi*t*4492.8*10^6);
 
 rPhi = y;
