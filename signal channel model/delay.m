dd6719fda26cd04e878b9f3c0364a49f42c8fc96
shinanligao <39136233+shinanligao@users.noreply.github.com>
2018-05-13 13:56:58 +0200

    Further commentation added (Tarek)

--- signal channel model/delay.m
+++ signal channel model/delay.m
@@ -8,22 +8,33 @@ l = length(input);
 % Round input to nearest integer: Only shifts of whole nanoseconds allowed
 delta = round(A);
 
+% Extract tail from end - delay to end of input array.
 tail = input(end-delta+1:end);
+
+% Special case: If delay is one, set tail to last element of input
+% vector.
 
 if (delta==1)
     tail = input(end);
 end
 
+% Shift the input array delta indices to the right.
+% The elements at the end of input array are lost, but are stored in tail
+% vector.
 
 for k = 0:(l-1-delta)
     input(l - k) = input(l - k - delta);
 end
 
+% Fill out the obsolete indices at the left side of array with
+% zeros.
+
 for j = 1:delta
     input(j) = 0;
 end
 
-    
+% The delayed array is won by putting input and tail array together.
+
 delayed = cat(2, input, tail);
 
 end
